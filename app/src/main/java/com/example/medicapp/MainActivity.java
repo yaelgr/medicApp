76558c120550c1b1b3da9c6b62fd681e41e9d6dc
yaelgr <54602931+yaelgr@users.noreply.github.com>
2019-10-10 15:39:22 -0500

    xd

---
+++ app/src/main/java/com/example/medicapp/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.medicapp;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, AlarmaFragment.OnFragmentInteractionListener, MedicoFragment.OnFragmentInteractionListener,
+        MedicamentoFragment.OnFragmentInteractionListener, HistorialFragment.OnFragmentInteractionListener, ConfiguracionFragment.OnFragmentInteractionListener,
+        ContenedorAlarma.OnFragmentInteractionListener, AgregarAlarmaFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       /** FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        }); */
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        if (Utilidades.validaPantalla==true){
+            Fragment fragment=new AlarmaFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_main,fragment).commit();
+            Utilidades.validaPantalla=false;
+        }
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        Fragment myfragment=null;
+        boolean fragmentselected=false;
+
+        if (id == R.id.nav_alarma) {
+            myfragment=new ContenedorAlarma();
+            fragmentselected=true;
+        } else if (id == R.id.nav_medico) {
+            myfragment=new MedicoFragment();
+            fragmentselected=true;
+        } else if (id == R.id.nav_medicamento) {
+            myfragment=new MedicamentoFragment();
+            fragmentselected=true;
+        } else if (id == R.id.nav_historial) {
+            myfragment=new HistorialFragment();
+            fragmentselected=true;
+        } else if (id == R.id.nav_configuracion) {
+            myfragment=new ConfiguracionFragment();
+            fragmentselected=true;
+        }
+
+        if (fragmentselected==true){
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_main,myfragment).commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/com/example/medicapp/Utilidades.java
@@ -0,0 +1,7 @@
+package com.example.medicapp;
+
+class Utilidades {
+
+    public static int rotacion=0;
+    public static boolean validaPantalla=true;
+}
